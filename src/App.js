9be7b4976d79ca76957fb1ca9ec9fe65ef3cc7e2
Coders Key <designer.afride@gmail.com>
2021-10-22 12:29:13 -0700

    display loader added

--- src/App.js
+++ src/App.js
@@ -5,28 +5,52 @@ import Header from './components/Shared/Header/Header';
 import Footer from './components/Shared/Footer/Footer';
 import NotFound from './components/NotFound/NotFound';
 import Developer from './components/Developer/Developer';
+import { useState } from 'react';
+import Loader from 'react-loader-spinner';
 
 function App() {
+  const [startLoader, setStartLoader] = useState({})
+  const [content, setContent] = useState({})
+  
+  setTimeout(() => {
+      setStartLoader({
+          display: 'none',
+      })
+      setContent({
+          display: 'contents',
+      })
+  }, 2000);
   return (
     <div className="App">
-      <Router>
-        <Header></Header>
-          <Switch>
-              <Route exact path="/">
-                 <Home></Home>
-              </Route>
-              <Route path="/home">
-                 <Home></Home>
-              </Route>
-              <Route path="/developer">
-                 <Developer></Developer>
-              </Route>
-              <Route path="*">
-                 <NotFound></NotFound>
-              </Route>
-          </Switch>
-        <Footer></Footer>
-      </Router>
+      <div style={startLoader} className="startLoader">
+          <Loader
+              type="Puff"
+              color="#18bc9c"
+              height={100}
+              width={100}
+              timeout={3000} //3 secs
+          />
+      </div>
+      <div style={content} className="content">
+        <Router>
+          <Header></Header>
+            <Switch>
+                <Route exact path="/">
+                  <Home></Home>
+                </Route>
+                <Route path="/home">
+                  <Home></Home>
+                </Route>
+                <Route path="/developer">
+                  <Developer></Developer>
+                </Route>
+                <Route path="*">
+                  <NotFound></NotFound>
+                </Route>
+            </Switch>
+          <Footer></Footer>
+        </Router>
+      </div>
     </div>
   );
 }
